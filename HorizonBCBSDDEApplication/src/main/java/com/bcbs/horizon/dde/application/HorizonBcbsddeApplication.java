4622dbe39fc6ab2ffc6519dc2cd5c95e939e8fa2
nithya-ravindran <39188326+nithya-ravindran@users.noreply.github.com>
2018-05-11 16:10:51 +0530

    inital project structure

---
+++ HorizonBCBSDDEApplication/src/main/java/com/bcbs/horizon/dde/application/HorizonBcbsddeApplication.java
@@ -0,0 +1,12 @@
+package com.bcbs.horizon.dde.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonBcbsddeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonBcbsddeApplication.class, args);
+	}
+}
---
+++ HorizonBCBSDDEApplication/src/test/java/com/bcbs/horizon/dde/HorizonBcbsddeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcbs.horizon.dde;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HorizonBcbsddeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
